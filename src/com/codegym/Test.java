d0f51840e0fd385e4d5823724a58f6986b93231c
unknown <huenk105@gmail.com>
2022-03-29 10:30:27 +0700

    demo

---
+++ src/com/codegym/Test.java
@@ -0,0 +1,11 @@
+package com.codegym;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setClasses("C01");
+        student.setName("Daisy");
+
+        System.out.println(student);
+    }
+}
